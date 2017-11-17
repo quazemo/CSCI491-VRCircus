d863945e780d07c8c37c3e165cd3668c4097b6e2
Casron <32970184+Casron@users.noreply.github.com>
2017-11-16 19:11:25 -0800

    Add files via upload

---
+++ Assets/Scripts/Balloon.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Balloon : MonoBehaviour {
+
+	private Rigidbody rB;
+	void Start()
+	{
+		rB = gameObject.GetComponent<Rigidbody>();
+	}
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		rB.AddForce(0.0f,-10.0f * rB.mass, 0.0f, ForceMode.Force);
+	}
+}
---
+++ Assets/Scripts/Catapult.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Catapult : MonoBehaviour {
+
+	public GameObject leftPivot;
+	public GameObject rightPivot;
+}
